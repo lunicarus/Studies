8a49cc94a0c58c2d28f1de1833c7a8c5c2af59ee
lunicarus <65511597+lunicarus@users.noreply.github.com>
2021-12-20 16:50:52 -0300

    Tuples

--- Python/Chapter9.py
+++ Python/Chapter9.py
@@ -1,1 +1,8 @@
-#! Data structures are harder, so this chapter will have its on file
+
+#!Data structures are harder, so this chapter will have its on file
+#Chapter 9.1 exercises:
+    #cardinal_numbers = "first","second","third"; #creates a tuple packed with 3 values
+    #position1,position2,position3 = cardinal_numbers; #create variables that hold the values packed in the tuple
+    #print(position1,position2,position3); #*demonstration of packing and unpacking of variables
+    #name = tuple("lucas"); 
+    #print("x" in name);
