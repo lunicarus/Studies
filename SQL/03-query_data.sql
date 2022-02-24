6c9d57c17c4397d6013bf2e88f1d81de14509fd7
lucas silva cruz <lucas6jau@gmail.com>
2022-02-23 21:28:26 -0300

    SQL BRUH

--- SQL/03-query_data.sql
+++ SQL/03-query_data.sql
@@ -1,15 +1,19 @@
 -- merge related data
-SELECT 
-e.id AS event_id,
-e.name,
-e.date_planned,
-loc.title,
-loc.street,
-loc.house_number,
-loc.city_name,
-u.first_name,
-u.email
-    FROM events AS e
-INNER JOIN locations AS loc ON  e.locations_id = loc.id
-INNER JOIN events_user AS eu ON eu.events_id = e.id
-INNER JOIN users AS u ON eu.users_id = u.id;
+-- SELECT 
+-- e.id AS event_id,
+-- e.name,
+-- e.date_planned,
+-- loc.title,
+-- loc.street,
+-- loc.house_number,
+-- loc.city_name,
+-- u.first_name,
+-- u.email
+--     FROM events AS e
+-- INNER JOIN locations AS loc ON  e.locations_id = loc.id
+-- INNER JOIN events_user AS eu ON eu.events_id = e.id
+-- INNER JOIN users AS u ON eu.users_id = u.id;
+
+-- left join gives all data from the left(events) and the related data ON the condition defined,
+-- SELECT * FROM locations as loc
+-- LEFT JOIN events as e ON  e.locations_id = loc.id; 
---
+++ SQL/05-more_query.sql
@@ -0,0 +1,5 @@
+-- SELECT c.name, COUNT(loc.id) FROM cities AS c
+-- LEFT JOIN locations as loc ON loc.city_name = c.name
+-- GROUP BY c.name
+-- HAVING COUNT(loc.id) >1;
+
