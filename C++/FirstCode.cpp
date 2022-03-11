9568cdf543c52c7ee70a3e528043f551cb4d4b8d
lucas <lucas6jau@gmail.com>
2022-03-11 19:06:47 -0300

    a

--- C++/FirstCode.cpp
+++ C++/FirstCode.cpp
@@ -1,23 +1,24 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
-double power(double num, double ex){
+double power(double num, double ex)
+{
     double tt = num;
-    for(int i=1;i < ex;i++){
-        num = num*tt;
-
+    for (int i = 1; i < ex; i++)
+    {
+        num = num * tt;
     }
-        return num;
+    return num;
 }
 int main()
 {
     int num;
     int ex;
-    std:string name;
-    std:: cout << "Hello USER, tell me your name" << std::endl;
-    std::getline(std::cin,name);
+    std::string name;
+    std::cout << "Hello USER, tell me your name" << std::endl;
+    std::getline(std::cin, name); //&& way to get strings with whitespaces
     std::cout << "Nice to meet you " << name << ", please inform a number and a exponential" << std::endl;
-    std::cin >> num >> ex;
-    std:: cout << "The result is " << power(num,ex) << std::endl;
+    std::cin >> num >> ex; //&& multiple inputs
+    std::cout << "The result is " << power(num, ex) << std::endl;
     return 0;
 }
