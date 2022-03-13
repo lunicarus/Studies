8200e1b65f8543aa09f22488b0c9407757793500
lucas <lucas6jau@gmail.com>
2022-03-13 12:06:22 -0300

    SSS

--- C/NewStudy/nestedpow.c
+++ C/NewStudy/nestedpow.c
@@ -3,11 +3,19 @@
 #include <math.h>
 
 int main(){
-    int i;
+    int i,j;
     double total,exponencial = 3;
-    double numero[5] = {2,3,6,9,10};
-    for(i = 0; i<5;i++){
-        total = pow(numero[i],exponencial);
-        printf("%.2lf\n", total);
+    double numero[2][5] = {
+        {2,3,6,9,10} , 
+        {4,7,12,15,11}
+    };
+    for(i = 0; i<2;i++){
+        printf("\n");
+        for(j = 0; j < 5;j++){
+            total = pow(numero[i][j],exponencial);
+            printf("%.2lf  ", total);
+        }
     }
+    printf("\n");
+    return 0;
 }
