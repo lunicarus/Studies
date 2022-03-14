a886c49bf30f51a5a0a94a2dfa5be15d65b50d84
lucas <lucas6jau@gmail.com>
2022-03-14 13:32:09 -0300

    ExerciciosAula8

--- C/strcs/prova.c
+++ C/strcs/prova.c
@@ -7,7 +7,8 @@ int c, d, t;
  
 for (c = 0 ; c < ( n - 1 ); c++){
   for (d = 0 ; d < n - c - 1; d++){
-    if (list[d] > list[d+1]){         
+    if (list[d] > list[d+1]){  
+             
       t         = list[d];
       list[d]   = list[d+1];
       list[d+1] = t;
