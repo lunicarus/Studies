3462fb5df061dd93992defa8f7abb632c0bdd523
lucas <lucas6jau@gmail.com>
2022-04-12 11:27:30 -0300

    itworks

---
+++ Ts/app.ts
@@ -0,0 +1,10 @@
+
+
+
+
+function Sum(x:number, y:number){
+    return x + y;
+}
+
+Sum(2,3); // returns 5
+console.log(Sum(4,5));
--- WebDesign/Js/HelloWorld.ts
+++
@@ -1,5 +0,0 @@
-//? simple commenting in TS
-
-console.log("HelloWorld!");
-const myStr = 'I am a \"double quoted\" string inside \"double quotes\"'; // Change this line
-console.log(myStr);
