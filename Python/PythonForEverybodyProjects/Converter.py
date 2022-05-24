2cfd223db4c005e03f03dd6dc4313da3c48e8b53
lucas <lucas6jau@gmail.com>
2022-05-24 17:47:22 -0300

    goodbyefrontend

---
+++ Python/PythonForEverybodyProjects/Converter.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def my_trig_sum(a, b):
+    """
+    function to demo return multiple
+    author
+    date
+    """
+    out1 = np.sin(a) + np.cos(b)
+    out2 = np.sin(b) + np.cos(a)
+    return out1, out2
+
+# a,b = my_trig_sum(2,3)
+
+# print(a)
+
+def convertor(operacoes=[0],resultado = False):
+    print(resultado)
+    print(operacoes)
+    
+def board(n):
+    zeros = np.zeros(n,n)
+    ones = np.ones(n,n)
+    for number in ones:
+        
