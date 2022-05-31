d2b7a86f3bc8842ec287e70bf3e5006f4c70254c
lucas <lucas6jau@gmail.com>
2022-05-31 17:12:22 -0300

    t

--- Python/Python_And_Numerical_methods/Chapter1.py
+++ Python/Python_And_Numerical_methods/Chapter1.py
@@ -1,17 +1,17 @@
 
-import math
-import numpy as np
-print((10*12)/2) #1.compute a triangle with base 10 and height 12
+# import math
+# import numpy as np
+# print((10*12)/2) #1.compute a triangle with base 10 and height 12
 
-# surface_area = 2*math.pi*3*5**2#2.Compute the surface area and volume of a cylinder with radius 5 and height 3.
-# volume = math.pi*5**2 + 2*math.pi*5*3
+# # surface_area = 2*math.pi*3*5**2#2.Compute the surface area and volume of a cylinder with radius 5 and height 3.
+# # volume = math.pi*5**2 + 2*math.pi*5*3
 
-# #3.Compute the slope between the points (3,4) and (5,9). y2−y1/x2−x1
-# x = (3,4)
-# y= (5,9)
-# print((y[1]-y[0])/(x[1]-x[0]))
+# # #3.Compute the slope between the points (3,4) and (5,9). y2−y1/x2−x1
+# # x = (3,4)
+# # y= (5,9)
+# # print((y[1]-y[0])/(x[1]-x[0]))
 
-#4
-y = np.array([[3,4,5],[1,9,2]])
-x = np.array([[1,1,1],[2,3,4]])
-print(np.average(x))
+# #4
+# y = np.array([[3,4,5],[1,9,2]])
+# x = np.array([[1,1,1],[2,3,4]])
+# print(np.average(x))
