ec7852437ce122db1455d6e273160174ad20b7b9
lucas <lucas6jau@gmail.com>
2022-07-09 11:40:39 -0300

    nice

--- Python/Python Basics/opening_files.py
+++
@@ -1,6 +0,0 @@
-xfile = open('/home/lucas/Documentos/Studies/Python/conhecimentos.txt')
-count = 0
-for n in xfile:
-    count += 1
-
-print(count)
--- Python/Python Basics/python_studies.py
+++
@@ -1,2 +0,0 @@
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(f"\n{thislist[2:5]}\n")
